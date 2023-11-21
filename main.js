7e43943a9a8769018b9d5cb66a43e67c8e9412f1
Luka Nozadze <nozadzeluka2002@gmail.com>
2023-11-21 16:46:44 +0400

    fixing the nav and adding the blobs

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primary-navigation');
 
 navToggle.addEventListener('click',()=>{
+    console.log('si');
     primaryNav.hasAttribute('data-visible')
-    ?navToggle.setAttribute('aria-expanded',true):navToggle.
-    setAttribute('aria-expanded',false)
+    ?navToggle.setAttribute('aria-expanded',false):navToggle.
+    setAttribute('aria-expanded',true)
     primaryNav.toggleAttribute('data-visible')
     primaryHeader.toggleAttribute('data-overlay')
     
